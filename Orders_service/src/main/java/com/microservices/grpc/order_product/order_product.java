7d76cdc7213a8619697339de891e65959fbaa67c
Anou Oussama <107358588+Usmaexe@users.noreply.github.com>
2025-04-07 16:18:10 +0100

    update : adding the services

--- Orders_service/src/main/java/com/microservices/grpc/order_product/order_product.java
+++ Orders_service/src/main/java/com/microservices/grpc/order_product/order_product.java
@@ -1,13 +1,13 @@
-package com.orders_service;
+package com.microservices.grpc.order_product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrdersServiceApplication {
+public class order_product {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrdersServiceApplication.class, args);
+        SpringApplication.run(order_product.class, args);
     }
 
 }
--- Orders_service/src/test/java/com/microservices/orderproductTests.java
+++ Orders_service/src/test/java/com/microservices/orderproductTests.java
@@ -1,10 +1,10 @@
-package com.orders_service;
+package com.microservices;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OrdersServiceApplicationTests {
+class orderproductTests {
 
     @Test
     void contextLoads() {
